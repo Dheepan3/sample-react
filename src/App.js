d905e2bb78c02a31c88fe375cbd2beb9d5eee41a
Dheepan.S <dheepan-s@dheepan-s-VirtualBox>
2024-09-25 15:12:58 +0530

    again i changed the link in line no:14

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://in.pinterest.com/pin/637963103471997571/"
+          href="https://www.google.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
